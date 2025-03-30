d2bb6781a7d8b17f30a5bd53aa886d44fedfa4cb
Alexander <AlexChapovg@gmail.com>
2025-03-30 14:49:56 +0400

    add poetry

---
+++ src/views.py
@@ -0,0 +1,13 @@
+import json
+import logging
+import os
+from datetime import datetime
+
+import pandas as pd
+from dotenv import load_dotenv
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+load_dotenv("../.env")
+API_TOKEN = os.getenv("API_TOKEN")
